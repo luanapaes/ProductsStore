dfd665d67780d6d065671443f89e3b0e5dc19579
Luana Paes <luanafranca598@gmail.com>
2024-06-03 20:34:31 -0300

    add MatToolbar ao component Header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  @Input() firstListItem: string = "";
+  @Input() secondListItem: string = "";
+  @Input() thirdListItem: string = ""
 }
