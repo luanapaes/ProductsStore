d9c5796704bba05aadebf3041539a81e2062da4b
Luana Paes <luanafranca598@gmail.com>
2024-06-06 22:40:13 -0300

    adiciona card para mostrar o produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,13 +1,15 @@
-import { JsonPipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/products.interface';
 
+//Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
