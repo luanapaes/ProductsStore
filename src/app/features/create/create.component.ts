e4c4714871e2735e76dd6aa814461aa64f49df0e
Luana Paes <luanafranca598@gmail.com>
2024-06-11 23:04:00 -0300

    adiciona campos ao formulário de criação de produtos

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  formProduct = new FormGroup({
+    title: new FormControl<string>('',
+      {
+        nonNullable: true,
+        validators: Validators.required
+      }
+    )
+  });
 
+  onSubmit() {
+    if (this.formProduct.valid) {
+      console.log(this.formProduct.value.title)
+    }
+  }
 }
