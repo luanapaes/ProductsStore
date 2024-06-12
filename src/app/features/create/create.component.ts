011005938f9269c9b4a03f0f68a439add76b0efb
Luana Paes <luanafranca598@gmail.com>
2024-06-12 19:04:08 -0300

    adiciona mensagem de sucesso ao criar produto

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -4,7 +4,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ProductsService } from '../../shared/services/products.service';
+import { Router } from '@angular/router';
+import { ListComponent } from '../list/list.component';
 
 @Component({
   selector: 'app-create',
@@ -21,6 +24,10 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class CreateComponent {
   productService = inject(ProductsService)
+  matSnackBar = inject(MatSnackBar)
+  router = inject(Router)
+
+
   formProduct = new FormGroup({
     title: new FormControl<string>('',
       {
@@ -30,11 +37,19 @@ export class CreateComponent {
     )
   });
 
-  onSubmit(){
-    if(this.formProduct.valid){
+  onSubmit() {
+    if (this.formProduct.valid) {
       this.productService.createProduct({
         title: this.formProduct.controls.title.value
-      }).subscribe(() => alert("Sucesso!"))
+      }).subscribe(() => {
+        this.matSnackBar.open("Produto cadastrado com sucesso!", "Ok", {
+          duration: 3000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top'
+        })
+
+        this.router.navigateByUrl('/')
+      })
     }
   }
 }
