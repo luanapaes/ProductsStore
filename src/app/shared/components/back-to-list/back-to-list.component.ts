72976b2dbed20bdf6dcae89fddc1308951f34f08
Luana Paes <luanafranca598@gmail.com>
2024-06-26 22:13:48 -0300

    adiciona componente para navegar para listagem

---
+++ src/app/shared/components/back-to-list/back-to-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-back-to-list',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './back-to-list.component.html',
+  styleUrl: './back-to-list.component.scss'
+})
+export class BackToListComponent {
+
+}
